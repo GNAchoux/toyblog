b9d8f715cb7193348fa10befb018059f682645c7
GNAchoux <hi5373934@gmail.com>
2026-06-01 20:09:46 +0900

    cd: add  static_root

--- blogpractice/settings.py
+++ blogpractice/settings.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 from decouple import config
+import os
+
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -115,7 +117,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 try:
